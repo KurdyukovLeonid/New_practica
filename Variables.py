b14106aa8a32e8f28e8a9ace8a59c1dc03c074ba
kurdyukov <leonid.kurdyukov@icloud.com>
2024-06-23 13:16:03 +0300

    Переменные

---
+++ Variables.py
@@ -0,0 +1,9 @@
+number_of_homeworks_completed = 12
+number_of_hours_spent = 1.5
+name_of_the_course = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_homeworks_completed
+
+print(f'Курс: {name_of_the_course}, '
+      f'всего задач: {number_of_homeworks_completed}, '
+      f'затрачено часов: {number_of_hours_spent}, '
+      f'среднее время выполнения {time_for_one_task} часов.')
