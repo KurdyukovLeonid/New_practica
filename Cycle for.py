20c4c78a9fb90866a937c2bb4ab4ce2ef1703e33
kurdyukov <leonid.kurdyukov@icloud.com>
2024-06-24 10:33:25 +0300

    Цикл for. Элементы списка. Полезные функции в цикле

---
+++ Cycle for.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_prime = 0
+
+for i in range(2, len(numbers) + 1):
+    for j in range(1, i + 1):
+        if i % j == 0:
+            is_prime += 1
+    if is_prime == 2:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+    is_prime = 0
+
+print('Primes:', primes)
+print('Not primes:', not_primes)
