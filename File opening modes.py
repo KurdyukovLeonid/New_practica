41cea514ce5f757e1925d337c45b19d3bc489e82
kurdyukov <leonid.kurdyukov@icloud.com>
2024-07-30 11:41:24 +0300

    Режимы открытия файлов

---
+++ File opening modes.py
@@ -0,0 +1,43 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        return file.read()
+        file.close()
+
+    def add(self, *products: Product):
+        result = set()
+
+        file = open(self.__file_name, 'r')
+        for i in file:
+            result.add(i.split(',')[0])
+
+        for product in products:
+            if product.name in result:
+                print(f'Продукт {product.name} уже есть в магазине')
+            else:
+                file = open(self.__file_name, 'a')
+                file.write(str(product) + '\n')
+                result.add(product.name)
+                file.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+print(p2)
+s1.add(p1, p2, p3)
+print(s1.get_products())
