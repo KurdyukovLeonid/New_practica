209a8cb2ac7a96c71faf9d6eef591f9307b0153a
kurdyukov <leonid.kurdyukov@icloud.com>
2024-08-19 01:48:25 +0400

    Оператор with

---
+++ Operator with.py
@@ -0,0 +1,38 @@
+import string
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                content = file.read()
+                content = content.lower()
+                translator = str.maketrans('', '', string.punctuation + '')
+                cleaned_content = content.translate(translator)
+                words = cleaned_content.split()
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        all_words = self.get_all_words()
+        found_positions = {}
+        for file_name, words in all_words.items():
+            if word in words:
+                found_positions[file_name] = words.index(word)
+        return found_positions
+
+    def count(self, word):
+        all_words = self.get_all_words()
+        word_counts = {}
+        for file_name, words in all_words.items():
+            word_counts[file_name] = words.count(word)
+        return word_counts
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
